78446b6d125b4b1445a144feff7be8de1398389e
Ryan Anderson <ryan@rycander.com>
2019-07-01 00:04:19 -0700

    Complete basic post request

--- animehook.rb
+++ animehook.rb
@@ -2,12 +2,19 @@
 
 require 'uri'
 require 'net/http'
+require 'net/https'
 require 'json'
 
 def postPrepwork
-  uri = URI.parse getSecrets['testWebhook']
-  req = Net::HTTP::Post.new uri.path, initheader = {'Content-Type' => 'application/json'}
-  
+  uri = URI.parse getSecrets['test_webhook']
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
+  req = Net::HTTP::Post.new uri.path, initheader = {'Content-Type' =>'application/json'}
+  req.body = getBody
+  puts req.body
+  res = https.request(req)
+
+  puts "Response #{res.code} #{res.message}: #{res.body}"
 end
 
 def getSecrets
@@ -19,18 +26,34 @@ def getAnime
 end
 
 def getBody
-  body = {"content"=> "HAHAHAH! TIME FOR PREPWORK", "embeds"=> []}
-  anime = getAnime
-  anime.each do |item|
+  body = {
+    "content"=> "HAHAHAH! TIME FOR PREPWORK!#{getTitle}",
+    "embeds"=> [getImageEmbed]
+  }
+  getAnime.each do |item|
     embed = {
-      "title"=>item.name,
-      "description"=>"Episode #{item.ep}"
-      "url"=>item.url,
+      "title"=>item["name"],
+      "description"=>"Episode #{item["ep"]}",
+      "url"=>item["url"],
       "thumbnail"=> {
-        "url"=>item.image
+        "url"=>item["img"]
       }
     }
+    body["embeds"].push embed
+  end
+  body.to_json
+end
 
+def getImageEmbed
+  {
+    "image" => {
+      "url": "https://cdn.discordapp.com/attachments/294543539553959947/592911897120997376/image0.jpg"
+    }
+  }
+end
+
+def getTitle
+  readJSONFile './titles.txt'
 end
 
 def readJSONFile path
@@ -40,3 +63,4 @@ def readJSONFile path
   data
 end
 
+postPrepwork
